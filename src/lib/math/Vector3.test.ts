917c0e766b78d858aeac9118e141d6471134ac9f
Diego Matias de Oliveira <dm.diego.bh@gmail.com>
2020-04-12 20:34:40 -0300

    added more tests

--- src/lib/math/Vector3.test.ts
+++ src/lib/math/Vector3.test.ts
@@ -52,3 +52,20 @@ test("should not be able to change zero value",()=>{
   expect(Vector3.zero.GetY()).toBe(0)
   expect(Vector3.zero.GetZ()).toBe(0)
 })
+
+test("should be abble to calculate length correctly",()=>{
+  const v = new Vector3(2,2,1)
+
+  expect(v.Length()).toBe(3)
+  expect(v.LengthSquared()).toBe(9)
+})
+
+test("should normalize correctly",()=>{
+  const v = new Vector3(2,2,1)
+
+  v.Normalize()
+
+  expect(v.GetX()).toBe(2/3)
+  expect(v.GetY()).toBe(2/3)
+  expect(v.GetZ()).toBe(1/3)
+})
